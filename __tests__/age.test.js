76d10d9d4635a10dc0f5f1bceac5688498cc754b
Joseph Murray <josephwmurray@gmail.com>
2023-03-25 16:43:14 -0700

    establish second red condition

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -10,6 +10,9 @@ describe('Age', () => {
 
   test('should calculate the age of a user in days', () => {
     expect(userAge.earthAge()).toEqual(14600);
-
   });
+
+  test('should calculate the age of a user in venus years', () => {
+    expect(userAge.venusAge()).toEqual(165);
+  })
 });
